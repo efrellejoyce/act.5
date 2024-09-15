ca0f503aa69a8e4ca0c85da1cde77ecd306da3a1
Efrelle Joyce Ducyao <efrellejoyced@gmail.com>
2024-09-15 12:39:15 +0700

    initial commit with strings.js

---
+++ strings.js
@@ -0,0 +1,14 @@
+let favActorFirstName = "Marian";
+let favActorLastName = "Dantes";
+let fullName = favActorFirstName + " " + favActorLastName;
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+
+let uppercase = "marian dantes";
+console.log(uppercase.toUpperCase());
+let lowercase = "PENGUIN";
+console.log(lowercase.toLowerCase());
+
+let message = "My favorite actor is " + (fullName.toUpperCase()) + "her best show is Marimar";
+console.log(message);
